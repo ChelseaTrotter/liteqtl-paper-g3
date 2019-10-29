69f8854e68743d6c01789cc3f3747e440d512aa8
Xiaoqi Hu <xiaoqihu@uthsc.edu>
2019-10-29 11:59:35 -0500

    modified impact of using Julia section, modifed example code, and added reference to cublas example

--- code/gpu.jl
+++ code/gpu.jl
@@ -1,18 +1,33 @@
-# Calling GPU BLAS library
-CuArrays.CUBLAS.gemm('T', 'N', a,b);
+## Example 1:
 
-# Writing custom kernel for calculating LOD score 
-# from correlation matrix
-function lod_kernel(input, MAX,n)
+A = rand(1000,1000)
+B = rand(1000,1000)
+# Data transfer from CPU to GPU
+d_a = CuArray(A)
+d_b = CuArray(B)
+# Do matrix multiplication on GPU by calling CuBLAS library.
+d_c = CuArrays.CUBLAS.gemm('T', 'N', d_a,d_b);
+# Data Transfer from GPU to CPU
+C = collect(d_c)
+
+## Example 2:
+
+# Custom kernel to do a matrix element-wise calculation
+function log_kernel(data, MAX)
+  # calculating GPU thread ID
   i = (blockIdx().x-1) * blockDim().x + threadIdx().x
+  # Check thread ID is in bound.
   if(i < MAX+1)
-    r_square = (input[i]/n)^2
-    input[i] = (-n/Float64(2.0)) *  
-               CUDAnative.log(Float64(1.0)-r_square)
+    # Call log function on GPU
+    data[i] = CUDAnative.log(data[i])
   end
   return
 end
 
+# initialize and transfer data to GPU.
+MAX = 64000
+d_data = CuArray(rand(MAX))
 # Launching GPU
-@cuda blocks=1000 threads=64 
-lod_kernel(input, MAX, n)
+d_res = @cuda blocks=1000 threads=64 log_kernel(data, MAX)
+# Transfer result back to CPU
+res = collect(d_res)
